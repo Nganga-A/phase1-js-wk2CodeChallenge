0ce6c194e6da2b322d860f9d920dc80c7eabf17e
Abed Nganga <ngangaabed@gmail.com>
2023-06-24 11:01:26 +0300

    2nd Commit - resetting votes,form,update readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # phase1-js-wk2CodeChallenge
+CHALLENGE DESCRIPTION
+Implement a mini web app to practice on array iteration, DOM, Events, and Server communications.
+For this challenge, you will be working on Flataculties, an app where you can vote for the cutest animal! You will use a local API and build out the frontend for our app, Flatacuties.
+
+DELIVERABLES :
+1)See a list of all animal names.Make a GET request to the following endpoint to retrieve   the character data
+2)Click on an animal’s name to see its details i.e image and number of votes. Note, you should only be displaying the details of one animal at a time. You can either use the character information from your first request or make a new request to this endpoint to get the character's details 
+3)When viewing an animal’s details, I should be able to add the number of votes for each animal. This number of votes should then be displayed together with the animal’s details. No persistence is needed for the votes.
+
+
+
+PROJECT STRUCTURE/SET-UP
+My project has the following structure
+index.html - main HTML file
+script.js - main js file
+README.md - this README file
+
+GETTING STARTED
+To run the code challenge solution locally, follow these steps:
+1)Clone the repository
+2)Open the index.html file in a web browser.
+3)Interact with the DOM  by clicking on the animal name, adding animals using the form, voting and resetting votes.
+
+AUTHOR and LICENSE
+Author - Abed Nganga Njuguna
+This code challenge student repo is open-source and distributed under the MIT License.
--- script.js
+++ script.js
@@ -1,47 +1,211 @@
+let characters = [];
+
+const animalList = document.getElementById('aList');
+const animalContainer = document.getElementById('animal-container');
+
+
+// Function to display animal details
+function displayAnimalDetails(character) {
+  animalContainer.innerHTML = ''; // Initially empty when not clicked
+
+  // Display animal details when clicked
+  const animalImage = document.createElement('img');
+  animalImage.src = character.image;
+  animalContainer.appendChild(animalImage);
+
+  const votes = document.createElement('p');
+  votes.textContent = `Votes: ${character.votes}`;
+  animalContainer.appendChild(votes);
+
+  // Add votes
+  const voteButton = document.createElement('button');
+  voteButton.textContent = 'Vote';
+  voteButton.addEventListener('click', () => {
+    character.votes++;
+    votes.textContent = `Votes: ${character.votes}`;
+  });
+
+  // Reset votes
+  const voteResetButton = document.createElement('button');
+  voteResetButton.textContent = 'Reset Votes';
+  voteResetButton.addEventListener('click', () => {
+    character.votes = 0;
+    votes.textContent = `Votes: ${character.votes}`;
+  });
+
+  animalContainer.appendChild(voteButton);
+  animalContainer.appendChild(voteResetButton);
+}
+
+// Function for event listener & changing cursor to pointer
+function MouseEventListener(element, character) {
+  element.addEventListener('click', () => {
+    displayAnimalDetails(character);
+  });
+
+  element.style.cursor = 'pointer';
+}
+
+fetch('http://localhost:3000/characters')
+  .then(response => response.json())
+  .then(characters => {
+
+
+    // Display list of animal names
+    characters.forEach(character => {
+      const animalName = document.createElement('li');
+      animalName.textContent = character.name;
+      animalList.appendChild(animalName);
+
+      MouseEventListener(animalName, character);
+    });
+  })
+
+
+// Form
+document.getElementById('animal-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const nameInput = document.getElementById('characterName');
+  const imageInput = document.getElementById('characterImage');
+
+  const newCharacter = {
+    name: nameInput.value,
+    image: imageInput.value,
+    votes: 0,
+  };
+
+  characters.push(newCharacter);
+
+  const newAnimalName = document.createElement('li');
+  newAnimalName.textContent = newCharacter.name;
+  animalList.appendChild(newAnimalName);
+
+  MouseEventListener(newAnimalName, newCharacter);
+
+  // Reset input for other submissions
+  nameInput.value = '';
+  imageInput.value = '';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//CODE BEFORE USING FUNCTIONS
+
+
+let characters = [];
+
+const animalList = document.getElementById('aList');
+const animalContainer = document.getElementById('animal-container')
+
+
+
+
 fetch(`http://localhost:3000/characters`)
 .then(response => response.json())
 .then(characters => {
      
-    const characterList = document.getElementById('aList');
     
     //Display list of animal names
     characters.forEach(character => {
        const animalName = document.createElement('li')
        animalName.textContent = character.name;
-       characterList.append(animalName);
+       animalList.appendChild(animalName);
 
+       //change cursor to pointer
+       animalName.addEventListener('mouseover', function() {
+        animalName.style.cursor = 'pointer';
+        });
+       
+       animalName.addEventListener('click', () => {
+        animalContainer.innerHTML = ''; //initially empty
 
-       //Display animal details 
-        fetch(`http://localhost:3000/characters/${character.id}`)
-        .then(response => response.json())
-        .then(animal => {
-            const animalDetails = document.getElementById('animal-container')
+        //Display animal details when clicked
+        const animalImage = document.createElement('img');
+        animalImage.src = character.image;
+        animalContainer.appendChild(animalImage);
 
-            const animalName = document.createElement('span')
-            animalName.textContent = `Name: ${animal.name}`
+        const votes = document.createElement('p');
+        votes.textContent = `Votes: ${character.votes}`
+        animalContainer.appendChild(votes)
+        
+        //add votes
+        const voteButton = document.createElement('button')
+        voteButton.textContent = 'Vote';
+        voteButton.addEventListener('click', () => {
+            character.votes++;
+            votes.textContent = `Votes: ${character.votes}`
+        });
 
-            const animalImage = document.createElement('img')
-            animalImage.src = animal.image;
-
-            const votes = document.createElement('p');
-            votes.textContent = `Number of Votes: ${animal.votes}`
-            
-            animalDetails.appendChild(animalName)
-            animalDetails.appendChild(animalImage)
-            animalDetails.appendChild(votes)
-           
-
-            //vote button
-            const voteButton = document.createElement('button')
-            voteButton.textContent = 'Vote';
-            voteButton.addEventListener('click', () => {
-                animal.votes++;
-                votes.textContent = `Number of Votes: ${animal.votes}`
-               // animalDetails.appendChild(voteButton)
-
-            });
-
-           animalDetails.appendChild(voteButton)
+        //reset votes
+        const voteResetButton = document.createElement('button'); 
+        voteResetButton.textContent = 'Reset Votes'
+        voteResetButton.addEventListener('click', () => {
+            character.votes = 0;
+            votes.textContent = `Votes: ${character.votes}`
         })
+
+        animalContainer.appendChild(voteButton)
+        animalContainer.appendChild(voteResetButton)
+
+       });
+
+
     });
 })
+
+//form
+document.getElementById('animal-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const nameInput = document.getElementById('characterName')
+    const imageInput =document.getElementById('characterImage')
+
+    const newCharacter = {
+        name: nameInput.value,
+        image: imageInput.value,
+        votes:0
+    }
+
+    characters.push(newCharacter)
+    const newAnimalName = document.createElement('li');
+    newAnimalName.textContent = newCharacter.name;
+    animalList.appendChild(newAnimalName);
+
+    newAnimalName.style.cursor = 'pointer';
+
+    newAnimalName.addEventListener('click',() => {
+          displayAnimalDetails(newCharacter)
+    })
+
+    //reset input for other submissions
+    nameInput.value = '';
+    imageInput.value = '';
+})
+
+*/
